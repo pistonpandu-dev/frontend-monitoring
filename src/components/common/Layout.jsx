f87cf2d1a67e4997b744601c4fe5f4c36db02175
pistonpandu-dev <pistonpandu@gmail.com>
2026-07-20 21:09:01 +0700

    Create Layout.jsx

---
+++ src/components/common/Layout.jsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import Sidebar from './Sidebar';
+import Navbar from './Navbar';
+
+const Layout = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(true);
+
+  return (
+    <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
+      <Sidebar open={sidebarOpen} setOpen={setSidebarOpen} />
+      <div className={`transition-all duration-300 ${sidebarOpen ? 'ml-64' : 'ml-20'}`}>
+        <Navbar toggleSidebar={() => setSidebarOpen(!sidebarOpen)} />
+        <main className="p-6">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
